85856fa2294ac5134ab38d6deaf11f7b7eedfb50
8949897221 <vanshyadav2808@gmail.com>
2025-02-01 10:32:18 +0530

    Add age classification feature and comment out normalization function

--- normal.py
+++ normal.py
@@ -1,10 +1,22 @@
-def normalize(data):
-    min_val = min(data)
-    max_val = max(data)
-    range_val = max_val - min_val
-    return [(x - min_val) / range_val for x in data]
+# def normalize(data):
+#     min_val = min(data)
+#     max_val = max(data)
+#     range_val = max_val - min_val
+#     return [(x - min_val) / range_val for x in data]
 
-if __name__ == "__main__":
-    sample_data = [10, 20, 30, 40, 50]
-    normalized_data = normalize(sample_data)
-    print("Normalized Data:", normalized_data)
+# if __name__ == "__main__":
+#     sample_data = [10, 20, 30, 40, 50]
+#     normalized_data = normalize(sample_data)
+#     print("Normalized Data:", normalized_data)
+
+
+    #data types
+a=int(input("Enter your age: "))
+if(a<13):
+    print("You are a child")
+elif(a>=13 and a<=19):
+    print("You are a teenager")
+elif(a>=20 and a<=59):
+    print("You are an adult")
+else:
+    print("You are a senior citizen")
