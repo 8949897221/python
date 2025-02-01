8bbb20b275b892380204756306ae2490a505732d
8949897221 <vanshyadav2808@gmail.com>
2025-02-01 10:36:18 +0530

    Add grading system based on score input

---
+++ movieticket.py
@@ -0,0 +1,12 @@
+a=int(input("Enter your score:"))
+if(a<=100 and a>=90):
+    print("You get grade A")
+elif(a<=89 and a>=80):
+    print("You get grade B")
+elif(a>=79 and a<=70):
+    print("You get grade C")
+elif(a>=69 and a<=60):
+    print("You get grade D")
+else:
+    print("You get grade F")
+    
